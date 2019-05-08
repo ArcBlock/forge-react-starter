7f8806225b3b2aa6394d3616ac7088ffa6cf8ac8
wangshijun <wangshijun2010@gmail.com>
2019-05-08 18:27:40 +0800

    chore: basic running next.js + express

--- app.js
+++ app.js
@@ -21,27 +21,27 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Create next.js application
-// const app = next({
-//   dev: process.env.NODE_ENV !== 'production',
-//   dir: path.join(__dirname, './client'),
-// });
+const app = next({
+  dev: process.env.NODE_ENV !== 'production',
+  dir: path.join(__dirname, './client'),
+});
 
 // Prepare next.js application
-// app.prepare().then(() => {
-//   const handle = app.getRequestHandler();
+app.prepare().then(() => {
+  const handle = app.getRequestHandler();
 
-// Create and config express application
-const server = express();
-server.use(cors());
+  // Create and config express application
+  const server = express();
+  server.use(cors());
 
-// eslint-disable-next-line global-require
-require('./server/routes')(server);
+  // eslint-disable-next-line global-require
+  require('./server/routes')(server);
 
-// server.get('*', (req, res) => handle(req, res));
+  server.get('*', (req, res) => handle(req, res));
 
-const port = parseInt(process.env.PORT, 10) || 3000;
-server.listen(port, err => {
-  if (err) throw err;
-  console.log(`> ${name} v${version} ready on http://localhost:${port}`);
+  const port = parseInt(process.env.PORT, 10) || 3000;
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> ${name} v${version} ready on http://localhost:${port}`);
+  });
 });
-// });
