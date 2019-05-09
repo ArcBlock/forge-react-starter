3975e2a9bda1a826d73fedcb867f9f03813a540d
wangshijun <wangshijun2010@gmail.com>
2019-05-09 14:51:35 +0800

    chore: update home page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,32 +1,90 @@
 import React from 'react';
 import styled from 'styled-components';
+import Link from 'next/link';
+
 import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 
 import Layout from '../components/layout';
+
+const demos = [
+  {
+    title: 'Login',
+    subtitle: 'Example 1',
+    description:
+      'Use ABT Wallet to login to an application built on top of a forge powered blockchain, and persist user info in the session',
+    link: '/profile',
+  },
+  {
+    title: 'Checkin',
+    subtitle: 'Example 2',
+    description: 'Help user to get some free tokens on the blockchain to test our application',
+    link: '/profile',
+  },
+  {
+    title: 'Payment',
+    subtitle: 'Example 3',
+    description: 'Allow user to pay for an secret document with crypto token, and records payment info in database.',
+    link: '/payment',
+  },
+];
 
 export default function IndexPage() {
   return (
     <Layout>
       <Main>
-        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-          Your Wallet, The Digital Way
+        <Typography component="h2" variant="h3" align="center" color="textPrimary" gutterBottom>
+          {process.env.appName}
         </Typography>
         <Typography variant="h6" align="center" color="textSecondary" component="p">
-          A decentralized wallet that puts you in control of everything - digital identity, profiles and more. Find out
-          who wants to see your data and manage what data you are willing to share. Get notified anytime someone makes a
-          request.
+          Application boilerplate built on top of <a href="https://www.arcblock.io/en/forge-sdk">forge</a> powered
+          blockchain, with developer friendly{' '}
+          <a href="https://docs.arcblock.io/forge/latest/sdk/javascript.html">forge javascript sdk</a>. Makes it super
+          easy to start building distributed applications with tons of thousands of react/javascript
+          libraries/components
         </Typography>
-        <img src="https://abt-wallet.netlify.com/static/5ab6559936f775f006a75eee19cce000/645bb/group-43.png" alt="" />
+        <div className="demos">
+          {demos.map(x => (
+            <Card key={x.title} className="demo">
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  {x.subtitle}
+                </Typography>
+                <Typography component="h2" variant="h5" gutterBottom>
+                  {x.title}
+                </Typography>
+                <Typography component="p" variant="subtitle1" gutterBottom>
+                  {x.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button component="a" to={x.link} size="small" color="primary">
+                  Tyr Now
+                </Button>
+              </CardActions>
+            </Card>
+          ))}
+        </div>
       </Main>
     </Layout>
   );
 }
 
 const Main = styled.main`
-  margin: 80px 0;
-  text-align: center;
+  margin: 80px 0 0;
 
-  img {
-    margin-top: 80px;
+  .demos {
+    margin: 50px 0;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    .demo {
+      width: 360px;
+      height: 240px;
+    }
   }
 `;
