90d4650b09b4ac98852fa95443d07f7f639c96e9
wangshijun <wangshijun2010@gmail.com>
2019-05-09 11:39:43 +0800

    chore: auto load models

--- server/models/index.js
+++ server/models/index.js
@@ -1,1 +1,10 @@
-module.exports.User = require('./user');
+const fs = require('fs');
+const path = require('path');
+const capitalize = require('lodash/capitalize');
+
+const models = fs.readdirSync(__dirname).filter(x => x !== 'index.js');
+models.forEach(x => {
+  const name = capitalize(path.basename(x, '.js'));
+  // eslint-disable-next-line
+  module.exports[name] = require(path.join(__dirname, x));
+});
