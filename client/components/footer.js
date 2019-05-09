6acdada14f3822d441e3f19eb1c997f9c716823a
wangshijun <wangshijun2010@gmail.com>
2019-05-09 13:53:29 +0800

    chore: add doc links for footer

--- client/components/footer.js
+++ client/components/footer.js
@@ -5,20 +5,49 @@ import Typography from '@material-ui/core/Typography';
 export default function Footer() {
   const footers = [
     {
-      title: 'Company',
-      description: ['Team', 'History', 'Contact us', 'Locations'],
+      title: 'SDK Documentation',
+      items: [
+        { title: 'Getting Started', link: 'https://docs.arcblock.io/forge/latest/' },
+        { title: 'GraphQLClient', link: 'https://docs.arcblock.io/forge/sdks/javascript/latest/GraphQLClient.html' },
+        {
+          title: 'Mcrypto',
+          link: 'https://docs.arcblock.io/forge/sdks/javascript/latest/module-@arcblock_mcrypto.html',
+        },
+        { title: 'DID/Auth', link: 'https://docs.arcblock.io/forge/sdks/javascript/latest/module-@arcblock_did.html' },
+        { title: 'GRpcClient', link: 'https://docs.arcblock.io/forge/sdks/javascript/latest/GRpcClient.html' },
+        {
+          title: 'Examples',
+          link: 'https://github.com/ArcBlock/forge-js/tree/master/packages/graphql-client/examples',
+        },
+      ],
     },
     {
-      title: 'Features',
-      description: ['Cool stuff', 'Random feature', 'Team feature', 'Developer stuff', 'Another one'],
+      title: 'Forge Documentation',
+      items: [
+        { title: 'Concepts', link: 'https://docs.arcblock.io/forge/latest/intro/concepts.html' },
+        { title: 'Configuration', link: 'https://docs.arcblock.io/forge/latest/core/configuration.html' },
+        { title: 'Transactions', link: 'https://docs.arcblock.io/forge/latest/txs/' },
+        { title: 'RPC', link: 'https://docs.arcblock.io/forge/latest/rpc/chain.html' },
+        { title: 'Documentation', link: 'https://docs.arcblock.io' },
+      ],
     },
     {
-      title: 'Resources',
-      description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
+      title: 'Forge Tools',
+      items: [
+        { title: 'Forge CLI', link: 'https://docs.arcblock.io/forge/latest/tools/forge_cli.html#install-forge-cli' },
+        { title: 'Forge WEB', link: 'https://docs.arcblock.io/forge/latest/tools/forge_web.html' },
+      ],
     },
     {
-      title: 'Legal',
-      description: ['Privacy policy', 'Terms of use'],
+      title: 'Batteries Included',
+      items: [
+        { title: 'Express', link: 'https://expressjs.com/' },
+        { title: 'Next.js', link: 'https://nextjs.org/' },
+        { title: 'React.js', link: 'https://reactjs.org/' },
+        { title: 'Material UI', link: 'https://material-ui.com/' },
+        { title: 'styled-components', link: 'https://www.styled-components.com/' },
+        { title: 'Mongodb/Mongoose', link: 'https://mongoosejs.com/' },
+      ],
     },
   ];
 
@@ -29,9 +58,15 @@ export default function Footer() {
           <Typography variant="h6" color="textPrimary" gutterBottom>
             {x.title}
           </Typography>
-          {x.description.map(item => (
-            <Typography key={item} variant="subtitle1" color="textSecondary">
-              {item}
+          {x.items.map(item => (
+            <Typography
+              key={item.title}
+              component="a"
+              href={item.link}
+              variant="subtitle1"
+              color="textSecondary"
+              target="_blank">
+              {item.title}
             </Typography>
           ))}
         </Grid>
