731d9a973fff89005ae8fd1fd9682ea9eac6467d
wangshijun <wangshijun2010@gmail.com>
2019-05-09 18:33:21 +0800

    chore: move brand to app_name in .env

--- client/next.config.js
+++ client/next.config.js
@@ -4,14 +4,13 @@ require('dotenv').config();
 const path = require('path');
 const withCSS = require('@zeit/next-css');
 const withCustomBabelConfigFile = require('next-plugin-custom-babel-config');
-const { brand } = require('../package.json');
 
 module.exports = withCSS(
   withCustomBabelConfigFile({
     babelConfigFile: path.join(__dirname, './babel.config.js'),
 
     env: {
-      appName: brand,
+      appName: process.env.APP_NAME,
       appId: process.env.APP_ID,
       chainHost: process.env.CHAIN_HOST,
       chainId: process.env.CHAIN_ID,
