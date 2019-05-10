130262908ac6258b9dbf319a0d319e77c99f6f70
wangshijun <wangshijun2010@gmail.com>
2019-05-11 06:53:20 +0800

    chore: debug serverless

--- server/functions/app.js
+++ server/functions/app.js
@@ -62,14 +62,20 @@ server.use(
 
 const router = express.Router();
 
+// eslint-disable-next-line global-require
+require('../routes')(router);
+
 router.get('/hello', (req, res) => {
   res.send('hello world');
 });
 
-// eslint-disable-next-line global-require
-require('../routes')(router);
+router.get('/api/routes', (req, res) => {
+  // eslint-disable-next-line no-underscore-dangle
+  res.json(router._router.stack.filter(r => r.route).map(r => r.route.path));
+});
 
-server.use(dev ? '/' : '/.netlify/functions/', router);
+// CUATION: be sure to change this if file renamed
+server.use('/.netlify/functions/app/', router);
 
 // Make it serverless
 exports.handler = serverless(server);
