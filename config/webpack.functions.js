4d503ab616664e6a0f12639fdce44bd791a99ee6
wangshijun <wangshijun2010@gmail.com>
2019-05-21 07:58:28 +0800

    chore: remove useless netlify-lambda config file

--- config/webpack.functions.js
+++
@@ -1,7 +0,0 @@
-// const nodeExternals = require('webpack-node-externals');
-const prod = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  optimization: { minimize: prod },
-  // externals: [nodeExternals()],
-};
