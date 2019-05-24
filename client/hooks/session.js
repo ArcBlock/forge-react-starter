c0c0180a75dcba51d5d810051702a4171a15b0dc
wangshijun <wangshijun2010@gmail.com>
2019-05-24 10:58:39 +0800

    fix: should not throw error in session hook #4

--- client/hooks/session.js
+++ client/hooks/session.js
@@ -3,11 +3,17 @@ import api from '../libs/api';
 import { removeToken } from '../libs/auth';
 
 async function fetchSession() {
-  const res = await api.get('/api/session');
-  if (res.status === 400) {
+  try {
+    const res = await api.get('/api/session');
+    if (res.status === 400) {
+      removeToken();
+    }
+    return res.data;
+  } catch (err) {
     removeToken();
   }
-  return res.data;
+
+  return {};
 }
 
 export default function useSession() {
